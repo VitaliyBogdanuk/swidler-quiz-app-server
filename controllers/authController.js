c4813d6e85a7a0b7479edd8ba08c29bc3efc29aa
Vitalii Bohdaniuk <vitaliibogdaniuk@MacBook-Pro-Vitalii-4.local>
2023-10-14 02:30:59 +0300

    fixed import User model

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const { User } = require('../models/');
 const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 const passport = require('passport');
