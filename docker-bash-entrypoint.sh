5d840893effa76bd91aef5419c5665495edb5e0f
Vitalii Bohdaniuk <vitaliibogdaniuk@MacBook-Pro-Vitalii-4.local>
2023-10-18 17:53:26 +0300

    updated migration deploy script

--- docker-bash-entrypoint.sh
+++ docker-bash-entrypoint.sh
@@ -6,6 +6,9 @@
 # Check if the RUN_MIGRATIONS_AND_SEEDS environment variable is set to "true"
 if [ "$RUN_MIGRATIONS_AND_SEEDS" = "true" ]; then
     # Run migrations
+    npm run db:migrate:drop
+
+    # Run migrations
     npm run db:migrate
 
     # Run seeds
