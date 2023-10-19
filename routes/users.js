1be2d1129f8b2f6bcaa9ae2771ff670b00af8119
OleksandrZhelihovskyi <zhelihovskii.aleksandr@gmail.com>
2023-10-19 21:54:23 +0300

    user to topic connection added

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { listUsers, createUser, readUser, updateUser, deleteUser } = require('../controllers/userController');
+const { listUsers, createUser, readUser, updateUser, deleteUser, updateUserTopics } = require('../controllers/userController');
 const { ensureAuthenticated } = require('../middlewares/auth');
 
 router.get('/users', ensureAuthenticated, listUsers);
@@ -8,5 +8,5 @@ router.post('/user', ensureAuthenticated, createUser);
 router.get('/user/:id', ensureAuthenticated, readUser);
 router.put('/user/:id', ensureAuthenticated, updateUser);
 router.delete('/user/:id', ensureAuthenticated, deleteUser);
-
+router.post('/updateUserTopics', ensureAuthenticated, updateUserTopics);
 module.exports = router;
