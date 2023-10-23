11ee22d74d1cb90ff4e9e130a10dbabceff7059f
Vitalii Bohdaniuk <vitaliibogdaniuk@MacBook-Pro-Vitalii-4.local>
2023-10-23 22:14:09 +0300

    remover registration for admin panel

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { register, login, forgotPassword, logout } = require('../controllers/authController');
 
-router.post('/register', register);
+// router.post('/register', register);
 router.post('/login', login);
 router.post('/forgotpassword', forgotPassword);
 router.get('/logout', logout);
