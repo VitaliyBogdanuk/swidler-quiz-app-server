8df3c09d0563258755222d7ee4f937dcab184e24
OleksandrZhelihovskyi <zhelihovskii.aleksandr@gmail.com>
2023-11-03 13:46:37 +0200

    fixed

--- seeders/20231020173422-seed-cheater-phones.js
+++ seeders/20231020173422-seed-cheater-phones.js
@@ -7,11 +7,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert(tableName, cheaterPhonesData, { returning: true });
     // Set autoincrement initial value after seeding
-    const result = await queryInterface.sequelize.query(`
-    SELECT * from "${tableName}"
-    ORDER BY id DESC
-    LIMIT 1;
-    `);
+    const result = await queryInterface.sequelize.query(`SELECT * from "${tableName}" ORDER BY id DESC LIMIT 1; `);
     const sequenceName = '"CheaterPhones_id_seq"';
     const restartWith = Number(result[0][0].id) + 1;
     await queryInterface.sequelize.query(`SELECT setval('${sequenceName}', ${restartWith}, false);`);
